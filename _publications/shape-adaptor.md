2582c3d4caf4417152276f3ca45460f996b114b7
Federico Perazzi <perazzi@adobe.com>
2020-07-08 08:57:58 -0700

    Add shape adaptor

---
+++ _publications/shape-adaptor.md
@@ -0,0 +1,18 @@
+---
+title: "Shape Adaptor: A Learnable Resizing Module"
+collection: publications
+date: 2020-07-02
+venue: ECCV
+city: Virtual
+state:
+teaser:
+thumbnail: "shape-adaptor.png"
+authors: "Shikun Liu, Zhe Lin, Yilin Wang, Jianming Zhang, Federico Perazzi, Edward Johns"
+bibtex:
+uri:
+arxiv:
+project:
+poster:
+data:
+---
+
--- thumbnails.sh
+++ thumbnails.sh
@@ -1,5 +1,5 @@
-mkdir -p /tmp/thumbnails-cgl-web
-convert -quality 100 -background white -alpha flatten -density 300 $1 /tmp/thumbnails-cgl-web/image.jpg
+mkdir  /tmp/thumbnails-cgl-web
+convert -quality 100 -background white  -density 300 $1 /tmp/thumbnails-cgl-web/image.jpg
 mogrify -resize 312x400! /tmp/thumbnails-cgl-web/image-0.jpg
 convert -page +4+4 /tmp/thumbnails-cgl-web/image-0.jpg -alpha set \
 					\( +clone -background black -shadow 160x3+4+4 \) +swap \
@@ -10,4 +10,4 @@ convert -page +4+4 /tmp/thumbnails-cgl-web/image-0.jpg -alpha set \
 # 					\( +clone -background black -shadow 160x3+4+4 \) +swap \
 # 					-background none -mosaic $2
 
-rm -rf /tmp/thumbnails-cgl-web
+# rm -rf /tmp/thumbnails-cgl-web
