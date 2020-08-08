da7762e55de0e08bf2845618bb176a8c7eb33585
Federico Perazzi <fperazzi@fb.com>
2020-08-08 12:42:21 -0700

    Update thumbnails

--- thumbnails.sh
+++ thumbnails.sh
@@ -1,6 +1,6 @@
 mkdir  /tmp/thumbnails-cgl-web
-# convert -quality 100 -background white -alpha flatten -density 300 $1 /tmp/thumbnails-cgl-web/image.jpg
-# mogrify -resize 312x400! /tmp/thumbnails-cgl-web/image-0.jpg
+convert -quality 100 -background white -alpha flatten -density 300 $1 /tmp/thumbnails-cgl-web/image.jpg
+mogrify -resize 312x400! /tmp/thumbnails-cgl-web/image-0.jpg
 convert -page +4+4 /tmp/thumbnails-cgl-web/image-0.jpg -alpha set \
 					\( +clone -background black -shadow 160x3+4+4 \) +swap \
 					-background none -mosaic $2
